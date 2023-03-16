fe7561a968da7567170b6badec6844aec5ec03c9
pedroasgDEV <pedro.asg@aluno.ufop.edu.br>
2023-03-15 22:24:21 -0300

    finish game struct

--- src/Jogo_da_Velha/Jogo.java
+++ src/Jogo_da_Velha/Jogo.java
@@ -28,6 +28,8 @@ public class Jogo {
     	System.out.print("Digite o nome do Jogador que será o \"O\": ");
     	Nome = input.nextLine();
     	Joga[1] = new Jogador(Nome, 'O');
+
+		input.close();
     }
     
     
@@ -36,5 +38,62 @@ public class Jogo {
 	
 	public void setTabuleiro(char tabuleiro[][]) { Tabuleiro = tabuleiro; }
 	public void setJoga(Jogador joga[]) { Joga = joga;}
+	
+	public boolean venceu(){
+		//Verifica horizontalmente
+		for(int i = 0; i < 3; i++)
+			if(Tabuleiro[i][0] == Tabuleiro[i][1] && Tabuleiro[i][0] == Tabuleiro[i][2])
+				for(Jogador j : Joga) 
+					if(j.getSinal() == Tabuleiro[i][0]){
+						j.addPontos();
+						System.out.println("O jogador \"" + j.getSinal() + "\" venceu" );
+						return true;
+					}
+		
+		//Verifica verticalmente
+		for(int i = 0; i < 3; i++)
+			if(Tabuleiro[0][i] == Tabuleiro[1][i] && Tabuleiro[0][i] == Tabuleiro[2][i])
+				for(Jogador j : Joga) 
+					if(j.getSinal() == Tabuleiro[0][i]){
+						j.addPontos();
+						System.out.println("O jogador \"" + j.getSinal() + "\" venceu" );
+						return true;
+					}
+		
+		//Verifica diagonal principal
+		if(Tabuleiro[0][0] == Tabuleiro[1][1] && Tabuleiro[0][0] == Tabuleiro[2][2])
+			for(Jogador j : Joga) 
+				if(j.getSinal() == Tabuleiro[0][0]){
+					j.addPontos();
+					System.out.println("O jogador \"" + j.getSinal() + "\" venceu" );
+					return true;
+				}
+		
+		//Verifica diagonal secundaria
+		if(Tabuleiro[0][2] == Tabuleiro[1][1] && Tabuleiro[0][2] == Tabuleiro[2][0])
+			for(Jogador j : Joga) 
+				if(j.getSinal() == Tabuleiro[0][2]){
+					j.addPontos();
+					System.out.println("O jogador \"" + j.getSinal() + "\" venceu" );
+					return true;
+				}
+		
+		return false;
+		
+	}
+	
+	public void restart(){
+		for(int i = 0; i < 3; i++)
+    		for(int k = 0; k < 3; k++) Tabuleiro[i][k] = '-';
+	}
+	
+	public void fazer_jogada(char s) {
+		Scanner input = new Scanner(System.in);
+		int i, j;
+		System.out.print("Digie uma cordenada jogador \"" + s + "\":");
+		i = input.nextInt();
+		j = input.nextInt();
+		input.close();
+	}
 	
 }
